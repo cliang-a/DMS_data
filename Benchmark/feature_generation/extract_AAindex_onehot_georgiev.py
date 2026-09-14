60ca7f04e45f6c33583ad230a89c1fcd51ac12ad
cliang-a <liac@zju.edu.cn>
2026-09-14 23:13:21 +0800

    extract_AAindex_onehot_georgiev.py

--- Benchmark/feature_generation/extract_AAindex_onehot_georgiev.py
+++ Benchmark/feature_generation/extract_AAindex_onehot_georgiev.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # 1. Define input file paths (Beta-lactamase dataset and data splits)
-FULL_CSV = os.path.abspath(os.path.join(current_dir, "../../Beta-lactamase_evopro_esm-score_with_seq.csv"))
+FULL_CSV = os.path.abspath(os.path.join(current_dir, "../../data1/ESM_Score/Beta-lactamase_evopro_esm-score_with_seq.csv"))
 TRAIN_CSV = os.path.abspath(os.path.join(current_dir, "../data_split/train_set_N96.csv"))
 TEST_CSV = os.path.abspath(os.path.join(current_dir, "../data_split/test_set_Remaining.csv"))
 
